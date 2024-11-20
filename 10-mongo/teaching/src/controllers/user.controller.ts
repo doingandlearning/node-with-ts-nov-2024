d2564228536940273a8fa63a4954a9de98bf4663
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-11-20 16:01:59 +0000

    adds mongo

--- 10-mongo/teaching/src/controllers/user.controller.ts
+++ 10-mongo/teaching/src/controllers/user.controller.ts
@@ -1,17 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../types/user.types";
+import UserModel from "../models/user.models";
 
-const users: User[] = [];
-
-function getAllUsers(req: Request, res: Response, next: NextFunction) {
+async function getAllUsers(req: Request, res: Response, next: NextFunction) {
   try {
-    res.send(users);
+    const allUsers = await UserModel.find();
+    res.send(allUsers);
   } catch (error) {
     res.status(400).json({ message: "Something went wrong." });
   }
 }
 
-function createNewUser(req: Request, res: Response) {
+async function createNewUser(req: Request, res: Response, next: NextFunction) {
   try {
     const user: User = req.body;
     if (!user.location || !user.name) {
@@ -19,18 +19,30 @@ function createNewUser(req: Request, res: Response) {
         .status(400)
         .json({ message: "You need to provide a name and location." });
     } else {
-      user.id = users.length + 1;
-      users.push(user);
-      res.json(user);
+      const newUser = new UserModel({
+        name: user.name,
+        location: user.location,
+        role: user.role,
+      });
+      await newUser.save();
+      res.json(newUser);
     }
   } catch (error) {
-    console.log(error);
-    res.status(400).json({ message: "Something went wrong." });
+    if (error instanceof Error) {
+      if (error.message.startsWith("E11000")) {
+        res
+          .status(400)
+          .send({ message: "You already have someone with that name." });
+        return;
+      }
+    }
+    next(error);
   }
 }
-function getUserById(req: Request, res: Response) {
+
+async function getUserById(req: Request, res: Response) {
   try {
-    const user = users.find((u) => u.id === parseInt(req.params.id));
+    const user = await UserModel.findById(req.params.id);
 
     if (!user) {
       res.status(404).json({ message: "User not found." });
@@ -41,30 +53,29 @@ function getUserById(req: Request, res: Response) {
     res.status(400).json({ message: "Something went wrong." });
   }
 }
-function updateUserById(req: Request, res: Response) {
-  try {
-    const userIndex = users.findIndex((u) => u.id === parseInt(req.params.id));
 
-    if (userIndex === -1) {
-      res.status(404).json({ message: "User could not be found." });
+async function updateUserById(req: Request, res: Response) {
+  try {
+    const user = await UserModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) {
+      res.status(404).json({ message: "User not found." });
     } else {
-      users[userIndex] = { ...users[userIndex], ...req.body };
-      res.send(users[userIndex]);
+      res.send(user);
     }
   } catch (error) {
     res.status(400).json({ message: "Something went wrong." });
   }
 }
 
-function deleteUserById(req: Request, res: Response) {
+async function deleteUserById(req: Request, res: Response) {
   try {
-    const userIndex = users.findIndex((u) => u.id === parseInt(req.params.id));
-
-    if (userIndex === -1) {
-      res.status(404).json({ message: "User could not be found." });
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: "User not found." });
     } else {
-      users.splice(userIndex, 1); // Changes the underlying array!
-      res.status(204).send();
+      res.status(204).json();
     }
   } catch (error) {
     res.status(400).json({ message: "Something went wrong." });
